823e80ee825135af85e8aaa7fb4c22eee40244b7
Samy <chebreksamy@gmail.com>
2024-12-02 16:30:45 +0100

    [feat] : initialize

---
+++ museum-app/src/PaintingDisplay.jsx
@@ -0,0 +1,11 @@
+function PaintingDisplay() {
+    return (
+        <>
+        <img src="https://lh3.googleusercontent.com/X7_CHCjksOZYu4gIGa45Edj1tMymdiz2o3pbL6HqqVEszWvPzrM6iIwHzaWNqgsWLcm7VmHCQyuQowWSSImQYLF8qW48zmZ-rx309F3c=s0" />
+        <p className="nom peinture">River Landscape with Riders</p>
+        <p className="peintre">Aelbert Cuyp</p>
+        </>
+    )
+
+}
+export default PaintingDisplay
